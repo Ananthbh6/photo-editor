fb91d51a8c5bab42ba6026051c77aa5e401d88ec
ananthbh6 <ananth@chestnote.com>
2017-12-28 16:30:45 +0100

    changed the size of the View when making it an image

--- Photo Editor/Photo Editor/UIView+Image.swift
+++ Photo Editor/Photo Editor/UIView+Image.swift
@@ -13,8 +13,8 @@ extension UIView {
      Convert UIView to UIImage
      */
     func toImage() -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.isOpaque, 0.0)
-        self.drawHierarchy(in: self.bounds, afterScreenUpdates: false)
+        UIGraphicsBeginImageContextWithOptions((self.superview?.bounds.size)!, self.isOpaque, 0.0)
+        self.drawHierarchy(in: (self.superview?.bounds)!, afterScreenUpdates: false)
         let snapshotImageFromMyView = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return snapshotImageFromMyView!
