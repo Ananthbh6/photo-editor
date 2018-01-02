575cbda762d2c0c5c114bead36d86b20a175188e
ananthbh6 <ananth@chestnote.com>
2018-01-02 18:25:40 +0100

    fixed auto rotate on uploading portrait images

--- Photo Editor/Photo Editor/PhotoEditor+Filters.swift
+++ Photo Editor/Photo Editor/PhotoEditor+Filters.swift
@@ -32,7 +32,22 @@ extension PhotoEditorViewController {
         
         // 5 - convert filtered CGImage to UIImage
         let filteredImage = UIImage(cgImage: outputCGImage!)
-        return filteredImage
+        return fixOrientation(img: filteredImage)
+    }
+    
+    func fixOrientation(img: UIImage) -> UIImage {
+        if (img.imageOrientation == .up) {
+            return img
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(img.size, false, img.scale)
+        let rect = CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height)
+        img.draw(in: rect)
+        
+        let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return normalizedImage
     }
     
     func resizeImage(image: UIImage) -> UIImage {
